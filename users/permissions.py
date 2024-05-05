956b4ad495cee579ed72a6a8c752071297b4c4af
Angelina Pan <angln.pnn@gmail.com>
2024-05-05 14:30:21 +0700

    Добавлены права для сотрудника.

---
+++ users/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsActive(BasePermission):
+    """
+    Проверка активности сотрудника.
+    """
+    def has_permission(self, request, view):
+        if request.user.is_staff and request.user.is_active:
+            return True
+        return False
+
+
+
